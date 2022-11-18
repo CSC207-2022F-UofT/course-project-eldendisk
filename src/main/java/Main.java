3dac27b7352b05b8d225e5e780968665a933a4a0
William Shen <96030236+Wiilly-B@users.noreply.github.com>
2022-11-17 21:55:02 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,34 @@
+/* This is our main class where the meat and potatoes of the program is executed */
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
+public class Main {
+    public static void main(String[] args) {
+        title();
+        EldenDisk.startFight();
+    }
+
+    public static void title() {
+        int width = 140;
+        int height = 80;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("SansSerif", Font.BOLD, 24));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString("Elden Disk", 10, 30);
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
+            }
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+            System.out.println(sb);
+        }
+    }
+}
