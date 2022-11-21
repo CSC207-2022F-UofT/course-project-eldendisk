b25ada69c9805e975cb4cc3c2c25acbebf2b12e1
William Shen <96030236+Wiilly-B@users.noreply.github.com>
2022-11-21 13:30:17 -0500

    Update Main
    Cleans up the main file after moving all the classes to the EldenDisk class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,11 @@ import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
-        title();
+        EldenDisk.LargeText("ELDEN RING");
         EldenDisk.startFight();
+        EldenDisk.EndGameDialogue();
+        EldenDisk.LargeText("THE END");
     }
 
-    public static void title() {
-        int width = 140;
-        int height = 80;
 
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 24));
-
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("Elden Disk", 10, 30);
-
-        for (int y = 0; y < height; y++) {
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < width; x++) {
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
-            }
-            if (sb.toString().trim().isEmpty()) {
-                continue;
-            }
-            System.out.println(sb);
-        }
-    }
 }
