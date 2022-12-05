5de1b0f084acd83cc6c94dc630aa08c2bf84eac9
Isabelle <isabelle.huang@mail.utoronto.ca>
2022-12-05 15:06:52 -0500

    Added Id attribute to TempEldenDisk

--- src/main/java/ui/TempEldenDisk.java
+++ src/main/java/ui/TempEldenDisk.java
@@ -6,6 +6,8 @@ public class TempEldenDisk {
     private Player player;
     private int gameLvl;
 
+    private int game_id;
+
     public TempEldenDisk() {
         this.player = (Player)PlayerCreation.CreateNewCharacter();
         this.gameLvl = 1;
@@ -15,13 +17,19 @@ public class TempEldenDisk {
 
     public void increaseGameLvl() {this.gameLvl++;}
 
-
+    public void SetLvl(int lv){
+        this.gameLvl = lv;
+    }
     public Player getPlayer() {
-        return player;
+        return this.player;
     }
 
     public void setPlayer(Player player) {
         this.player = player;
+    }
+
+    public int GetId () {
+        return this.game_id;
     }
 
 }
