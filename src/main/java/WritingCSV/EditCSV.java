8c2958cc162900fbfec1351483565e02917bb236
Isabelle <isabelle.huang@mail.utoronto.ca>
2022-12-06 12:17:04 -0500

    Altered DeleteLineById method

--- src/main/java/WritingCSV/EditCSV.java
+++ src/main/java/WritingCSV/EditCSV.java
@@ -1,80 +1,86 @@
 package WritingCSV;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class EditCSV {
 
     public static void Write(String line, String filepath) throws IOException {
         /*** Writes the line into the file provided, assuming it's already put in the csv form. i.e. It's already in
          * the form separated by comma, for example, "1, Magician, 90".
-          */
+         */
         FileWriter fw = new FileWriter(filepath, true);
         PrintWriter pw = new PrintWriter(fw);
         pw.println(line);
         pw.flush();
         pw.close();
     }
+
     public static void DeleteLineById(String filepath, int remove_line) throws IOException {
-        int position = 0;
+            String tempFile = "temp.txt";
+            File old = new File(filepath);
+            File newf = new File(tempFile);
+
+            try {
+                FileWriter fw = new FileWriter(tempFile, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter pw = new PrintWriter(bw);
+                Scanner sc = new Scanner(new File(filepath));
+
+                while (sc.hasNext()){
+                    String line = sc.nextLine();
+                    if ((int) line.charAt(0) != remove_line){
+                        pw.println(line);
+                    }
+
+                }
+                sc.close();
+                pw.flush();
+                pw.close();
+                old.delete();
+                File dump = new File(filepath);
+                newf.renameTo(dump);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+    }
+
+    public static void OverWrite(String filepath, String overwriting_info, int id) {
         String tempFile = "temp.txt";
         File old = new File(filepath);
         File newf = new File(tempFile);
-        String curr_line;
 
-        FileWriter fw = new FileWriter(filepath, true);
-        PrintWriter pw = new PrintWriter(fw);
+        try {
+            FileWriter fw = new FileWriter(tempFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            Scanner sc = new Scanner(new File(filepath));
 
-        FileReader fr = new FileReader(filepath);
-        BufferedReader br = new BufferedReader(fr);
+            while (sc.hasNext()){
+                String line = sc.nextLine();
+                if ((int) line.charAt(0) == id){
+                    pw.println(overwriting_info);
+                }
+                else{
+                    pw.println(line);
+                }
 
-        while ((curr_line = br.readLine())!= null && position != remove_line){
-            pw.println(curr_line);
-            position += 1;
-            curr_line = br.readLine();
+            }
+            sc.close();
+            pw.flush();
+            pw.close();
+            old.delete();
+            File dump = new File(filepath);
+            newf.renameTo(dump);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        curr_line = br.readLine();
-
-        while ((curr_line = br.readLine())!= null){
-            pw.println((curr_line));
-            curr_line = br.readLine();
-        }
-        fw.close();
-        pw.close();
-        fr.close();
-        br.close();
-        old.delete();
-        File dump = new File(filepath);
-        newf.renameTo(dump);
 
 
     }
-
-//    public static void DeleteLineById(String filepath, int remove_line) throws IOException {
-//        int position = 0;
-//        String tempFile = "temp.txt";
-//        File old = new File(filepath);
-//        File newf = new File(tempFile);
-//        String curr_line ;
-//        String data[];
-//
-//        FileWriter fw = new FileWriter(filepath, true);
-//        PrintWriter pw = new PrintWriter(fw);
-//
-//        FileReader fr = new FileReader(filepath);
-//        BufferedReader br = new BufferedReader(fr);
-//
-//        while( (curr_line= br.readLine()) != null && position != remove_line){
-//                pw.println(curr_line);
-//                position += 1;
-//            }
-//        fw.close();
-//            pw.close();
-//            fr.close();
-//            br.close();
-//            old.delete();
-//            File dump = new File(filepath);
-//            newf.renameTo(dump);
-//        }
-
-    }
+}
 
