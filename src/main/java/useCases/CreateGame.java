1c194af0d5383e4f43a948982974e63ca17be5df
Isabelle <isabelle.huang@mail.utoronto.ca>
2022-12-06 15:55:40 -0500

    Fixed LoadGame according to implementation of Player

--- src/main/java/useCases/CreateGame.java
+++ src/main/java/useCases/CreateGame.java
@@ -7,19 +7,14 @@ import java.io.IOException;
 
 
 
-public class CreateGame implements StartGame {
+public class CreateGame{
     /**
      * The Use Case which creates a new Game with the input Player.
-     *
-     * @param player in the User input.
-     * @param id of the Game.
      * @return A new Game with the input Player.
      * @throws IOException when Game could not be started.
      */
 
-    @Override
-    public TempEldenDisk StartGame(Player player, int id) throws IOException {
-        TempEldenDisk ret = new TempEldenDisk();
-        return ret;
+    public TempEldenDisk StartGame() throws IOException {
+        return new TempEldenDisk();
     }
 }
--- src/main/java/useCases/LoadGame.java
+++ src/main/java/useCases/LoadGame.java
@@ -6,17 +6,14 @@ import entities.*;
 import java.io.*;
 
 
-public class LoadGame implements StartGame{
+public class LoadGame{
     /**
      * The Use Case that loads an old Game which the User picked to start.
-     *
-     * @param player player in the Game
      * @param id of the Game
      * @return A Game to be started.
      * @throws IOException
      */
-    @Override
-    public TempEldenDisk StartGame(Player player, int id) throws IOException {
+    public TempEldenDisk StartGame(int id) throws IOException {
         int i = 0;
         String[] info = GameStorage.FindGame(id);
             if (Integer.parseInt(info[0]) != i){
